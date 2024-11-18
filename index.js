0696c90a211489d5b60ff693d2aca42b3f31c432
it-coder-01 <icoder936@gmail.com>
2024-11-18 14:24:06 +0500

    create three files HTML , CSS and JS And  make form

---
+++ index.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+
+function register(){
+    x.style.left = "-400px";
+    y.style.left = "-0px";
+}
+function login(){
+    x.style.left = "-0px";
+    y.style.left = "400px";
+}
